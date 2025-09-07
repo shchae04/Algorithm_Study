51740811b0579191d9f1f63485c8e629fe107213
채상혁 <shchae04@naver.com>
2025-09-07 12:08:09 +0900

    백준: 1010

--- src/study/boj/daily/boj1010.java
+++ src/study/boj/daily/boj1010.java
@@ -6,7 +6,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
- * 다리 놓기
+ * 백준 1010번 - 다리 놓기
+ * 
+ * 문제: 서쪽 N개 사이트와 동쪽 M개 사이트를 연결하는 다리 개수 구하기 (다리는 겹치면 안됨)
+ * 
+ * 알고리즘: 조합론 (Combination)
+ * - N개 중에서 M개를 선택하는 조합 문제: C(M, N)
+ * - 서쪽 N개를 동쪽 M개 중에서 선택하여 연결
+ * - 다리가 겹치지 않으려면 순서대로 연결해야 함 → 조합
+ * - 오버플로우 방지를 위해 곱하고 나누기를 반복
+ * 
+ * 시간복잡도: O(min(N, M-N))
+ * 공간복잡도: O(1)
  */
 public class boj1010 {
 
