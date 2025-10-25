0d48448e02ee87bbcac9f061d3d5dca8ea441fd6
채상혁 <shchae04@naver.com>
2025-10-25 21:05:34 +0900

    백준: 2475

--- src/study/boj/daily/boj2475.java
+++ src/study/boj/daily/boj2475.java
@@ -2,6 +2,18 @@ package study.boj.daily;
 
 import java.util.Scanner;
 
+/**
+ * 백준 2475번 - 검증수
+ * 
+ * 문제: 5개 고유번호 각각을 제곱한 수의 합을 10으로 나눈 나머지 구하기
+ * 
+ * 알고리즘: 단순 구현
+ * - 5개 숫자를 입력받아 각각 제곱하여 합계 구하기
+ * - 합계를 10으로 나눈 나머지 출력
+ * 
+ * 시간복잡도: O(1)
+ * 공간복잡도: O(1)
+ */
 public class boj2475 {
 
     public static void main(String[] args) {
