0892779a2ebfd7f4f28c0ce5d1d1ecb78d8bc9ee
채상혁 <shchae04@naver.com>
2025-11-02 20:41:39 +0900

    백준: 1516

--- src/study/boj/daily/boj1516.java
+++ src/study/boj/daily/boj1516.java
@@ -5,6 +5,21 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 1516번 - 게임 개발
+ * 
+ * 문제: 건물을 짓는 순서를 고려한 최소 시간 구하기 (위상 정렬)
+ * 
+ * 알고리즘: 위상 정렬 + 동적 계획법
+ * - 각 건물의 선행 건물들을 모두 완료한 후 건설 시작
+ * - 위상 정렬을 통해 의존성 해결
+ * - dp[i] = max(dp[선행건물]) + 건설시간[i]
+ * 
+ * 시간복잡도: O(V + E) (V: 건물 수, E: 의존성 관계 수)
+ * 공간복잡도: O(V + E)
+ * 
+ * 주의: 현재 코드는 미완성 상태
+ */
 public class boj1516 {
 
     public static void main(String[] args) throws IOException {
