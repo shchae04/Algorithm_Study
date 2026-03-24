89007606582ba23e74f32d8ba134236c406b19a2
채상혁 <shchae04@naver.com>
2026-03-24 23:48:16 +0900

    백준: 15552 (입출력 속도 최적화를 위한 BufferedReader 및 BufferedWriter 활용)

---
+++ src/study/boj/implementation/boj15552.java
@@ -0,0 +1,24 @@
+package study.boj.implementation;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A + B + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
